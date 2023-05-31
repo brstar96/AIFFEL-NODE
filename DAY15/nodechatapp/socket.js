b88483febc1f49b0921a15f9e77769f8b663c068
MYEONGGYULEE <brstar96@naver.com>
2023-05-31 21:52:33 +0900

    DAY15: 그룹 채팅방 기능 구현하기 (WIP)
    1. 현재 전체 chatHistory 스트림에 발송된 전체 메시지 수신 불가능 문제 있음
    2. 채팅방 join 정상작동, 채팅방 내에서 메시지 수/발신 안되는 문제 있음

--- DAY15/nodechatapp/socket.js
+++ DAY15/nodechatapp/socket.js
@@ -1,4 +1,4 @@
-// socker.io 패키지 참조 
+// socket.io 패키지 참조 
 const SocketIO = require('socket.io');
 
 // socket.js 모듈 기능 정의 
@@ -17,7 +17,7 @@ module.exports = (server, app)=>{
         socket.on("broadcast", function(msg){
             
             // io.emit("클라이언트 이벤트 수신기 이름 정의", 클라이언트 이벤트 수신기에 전달할 데이터)
-            // io.emit() 메소드는 서버 소켓(socker.js)과 연결된 모든 클라이언트 소켓에게 메시지 전달
+            // io.emit() 메소드는 서버 소켓(socket.js)과 연결된 모든 클라이언트 소켓에게 메시지 전달
             // 100개의 브라우저(사용자)가 socket.js와 연결되어 있다면 100개의 브라우저에게 동일한 메시지 전달
             io.emit('receiveAll', msg); // 모든 클라이언트에게 메시지 전송
         }) 
@@ -32,6 +32,27 @@ module.exports = (server, app)=>{
         // test2 정의 (수십개 파라미터를 주고받기 난감할때 JSON 사용)
         socket.on('test2', async(jsonData)=>{
             io.emit('clientEvent2', jsonData.nickName, jsonData.msg);
+        })
+
+        // 채팅방 입장 처리하기 
+        socket.on('entry', async(channelId, nickName)=>{
+            // 서버 소켓에서 채팅방을 'channelId'으로 생성 -> 방이 존재하면 그냥 사용, 없으면 connectionId를 roomId로 사용해 방 생성 
+            // 현재 entry 수신기를 호출하는 클라이언트의 connectionId를 해당 채팅방의 사용자로 추가 
+            socket.join(channelId)
+            
+            // 현재 서버 이벤트 수신기(entry)를 호출한 클라이언트에게만 메시지를 보내고 싶을때는 
+            // socket.emit('클라이언트 이벤트 수신기명', 클라이언트에 보낼 메시지 데이터) 메소드 사용
+            socket.emit('entryOk', `${nickName}님 환영합니다. (현재 채널: ${channelId})`); // 나한테만 전송
+            
+            // 지정한 채널(채팅방) 내 현재 접속자 제외 모든 채팅방 사용자에게 메시지 전송 (socket.to(roomId).emit())
+            socket.to(channelId).emit('entryOk', `${nickName}님이 입장하셨습니다.`) // 나빼고 전부 전송 
+
+        })
+
+        // 그룹채팅 서버 이벤트 수신기 기능 정의 
+        socket.on('send', async(data)=>{
+            // 지정 채널에만 메시지 전송 (나를 포함한 모든 접속자) -> io.to(channelId).emit('클라이언트 이벤트 수신기명', 클라이언트에 보낼 메시지 데이터)
+            io.to(data.channelId).emit('receiveGroupMsg', data)
         })
     })
 
